864e54fa95cd9e4442355f0791ee581ebf56efdb
David Woollard <dave@darve.co.uk>
2016-05-20 16:50:24 +0100

    Tweaks to the build system

---
+++ src/modules/othercomponent/manifest.js
@@ -0,0 +1,14 @@
+
+/**
+ * This file is used by Assemble to generate static HTML for each view state that is needed
+ */
+
+module.exports = {
+    name: 'othercomponent',
+    layout: 'body',
+    states: {
+        default: {
+            title: 'This is a totally different component'
+        }
+    }
+};
---
+++ src/modules/othercomponent/scripts/othercomponent.module.js
@@ -0,0 +1,6 @@
+
+'use strict';
+
+module.exports = function() {
+    console.log('sup dudes');
+};
